cd30875472eabaa034334378149f2252462cd8bd
Travis Long <tlong@mozilla.com>
2019-02-05 18:24:18 -0600

    Commit quiz 2

---
+++ iOS Quiz 2.playground/Contents.swift
@@ -0,0 +1,78 @@
+/*:
+ # iOS Development
+ ## Quiz 2
+ 
+ ### Instructions
+ * Enter your answer **BELOW** each question.
+ * Make sure you answer **EVERY** question, some later questions may require the results from previous questions.
+ * Read each question carefully, and answer the best you can.
+ * For questions that require code, yours **MUST** compile.  If you can't get it to work, put it in a comment block and you may get partial credit.
+ * For questions that are text answers, put your answers in comments (single line or block).
+ * When you are complete, save a copy of this playground named with **YOUR NAME**.
+ * Email your quiz to me by the end of class
+ * If you have any questions about the instructions or the test, please ask me for assistance.
+ */
+// LEAVE THIS ALONE (You may need the next couple of lines for your code to work)
+import UIKit
+import Foundation
+// LEAVE THIS ALONE
+//: ### Start Here
+//: Type your **first** and **last** name below
+
+
+//: ### Question 1
+//: Why are strings in Swift *not* indexed (subscripted) with integer values?
+
+
+//: ### Question 2
+//: Give an example of checking to see if a string starts with another string.
+
+
+//: ### Question 3
+//: Give an example of declaring an array of Ints using **initializer syntax**.
+
+
+//: ### Question 4
+//: Use the array you declared in question 3 and assign to it the values 1, 3, 5, 7, 9 using an **array literal**.
+
+
+//: ### Question 5
+//: Use the same array from question 4 and insert the value 4 in between 3 and 5.
+
+
+//: ### Question 6
+//: Use for-in to iterate over each value in your array of Ints and print them out.
+
+//: ### Question 7
+//: Give an example of declaring a dictionary that has an Int key and String value using **initializer syntax**
+
+//: ### Question 8
+//: Fix the error(s) in the following switch statement, and add the remaining values up to "9", "nine" "Nine"
+var justAnInt:Int
+var n:String = "8"
+switch n {
+case "1", "one", "One":
+    justAnInt = 1
+case "2", "two", "Two":
+    justAnInt = 2
+case "3", "three", "Three":
+    justAnInt = 3
+case "4", "four", "Four":
+    justAnInt = 4
+case "5", "five", "Five":
+    justAnInt = 5
+}
+
+
+//: ### Question 9
+//: Use the dictionary from question 7 and the loop below to add keys of 0 through 9 with their matching string as the value "0" through "9".
+for n in 0...9 {
+    // Your code here
+}
+
+//: ### Question 10
+//: Create a for-in loop that iterates over the elements in your dictionary and print out each key and value **on a separate line**
+
+//: ### Bonus Question
+//: What is source control and why would you want to use it?
+
